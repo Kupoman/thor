c32db17b19ef8bd5766b4b001d62f9a4252ccbf0
Daniel Stokes <kupomail@gmail.com>
2014-11-01 23:12:55 -0700

    Monsters: Making derived stats properties to keep them up to date

--- src/monster.py
+++ src/monster.py
@@ -1,6 +1,6 @@
 import techniques
 
-class Monster:
+class Monster(object):
 	def __init__(self, name="Monster", attack=20, defense=20, intelligence=20,
 			stamina=20, speed=20):
 		self.name = name
@@ -13,18 +13,54 @@ class Monster:
 		self.speed = speed
 
 		# Derived stats
-		self.damage = self.attack * 10
-		self.hp = self.defense * 10
-		self.command_count = self.intelligence
-		self.initiative = self.speed
-		self.recovery = 0.125*self.stamina
-		self.special_attack = 0.5 * (self.attack + self.intelligence)
-		self.special_defense = 0.5 * (self.defense + self.intelligence)
-		self.accuracy = 0.5 * (self.attack + self.speed)
-		self.evasion = 0.5 * (self.defense + self.speed) / 100.0
+			# self.damage
+			# self.hp
+			# self.command_count
+			# self.initiative
+			# self.recovery
+			# self.special_attack
+			# self.special_defense
+			# self.accuracy
+			# self.evasion
 
-		self.current_hp = self.hp
-		self.current_stamina = 50
+		self.current_hp = 0
+		self.current_stamina = 0
 
 		self.target = None
 		self.techniques = [techniques.Scratch]
+
+	@property
+	def damage(self):
+		return self.attack * 10
+
+	@property
+	def hp(self):
+		return self.defense * 10
+
+	@property
+	def command_count(self):
+		return self.intelligence / 5
+
+	@property
+	def initiative(self):
+		return self.speed
+
+	@property
+	def recovery(self):
+		return 0.125 * self.stamina
+
+	@property
+	def special_attack(self):
+		return 0.5 * (self.attack + self.intelligence)
+
+	@property
+	def sepcial_defense(self):
+		return 0.5 * (self.defense + self.intelligence)
+
+	@property
+	def accuracy(self):
+		return 0.5 * (self.attack + self.speed)
+
+	@property
+	def evasion(self):
+		return 0.5 * (self.defense + self.speed) / 100.0
