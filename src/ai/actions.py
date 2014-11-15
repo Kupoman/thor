4999dcd098f6bad2a9328b8958ebf140f1feb688
Daniel Stokes <kupomail@gmail.com>
2014-11-14 20:52:57 -0800

    Combat: No more beating a monster while they're down
    Monsters with no hp will no longer be targeted

--- src/ai/actions.py
+++ src/ai/actions.py
@@ -2,15 +2,15 @@ import random
 
 
 def use_tech(agent):
-	if not agent.targets:
+	targets = [t for t in agent.targets if t.hp > 0]
+	if not targets:
 		return
 
 	techniques = [t for t in agent.data.techniques if t.cost < agent.stamina]
-
 	if not techniques:
 		return
 
 	tech = random.choice(techniques)
-	target = random.choice(agent.targets)
+	target = random.choice(targets)
 
 	tech.use(agent, target)
